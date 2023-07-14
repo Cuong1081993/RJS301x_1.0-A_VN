703313100d1c5fee0e8e615e6fde59996bd0b73f
Cuong <ntqcuong1993@gmail.com>
2023-07-14 15:37:56 +0700

    remove

--- Lab 9/src/components/Meals/MealItem/MealItemForm.js
+++ Lab 9/src/components/Meals/MealItem/MealItemForm.js
@@ -1,23 +1,46 @@
-
-import Input from '../../UI/Input';
-import classes from './MealItemForm.module.css';
-
+import React, { useState, useRef } from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
 function MealItemForm(props) {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInutRef.current.value;
+    console.log(enteredAmount);
+    const enterdAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enterdAmountNumber === 0 ||
+      enterdAmountNumber > 5
+    ) {
+      return setAmountIsValid(false);
+    }
+    props.onAddToCart(enterdAmountNumber);
+  };
+  const amountInutRef = useRef();
   return (
-    <form className={classes.form}>
-        <Input label="Amount" input={{
-          id:"amount_" + props.id,
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+      
+        ref={amountInutRef}
+        label="Amount"
+        input={{
+          type: 'number',
+          id: "amount_" + props.id,
           tilte: "number",
-          min: '1',
+          min: "1",
           max: "5",
           step: "1",
-          defaultValue : "1"
-        }}/>
+          defaultValue: "1",
+        }}
+      />
 
-        <button>Add</button>
+      <button>+ Add</button>
+      {!amountIsValid && <p>Please enter valid amount(1-5)</p>}
     </form>
-  )
+  );
 }
 
-export default MealItemForm
+export default MealItemForm;
