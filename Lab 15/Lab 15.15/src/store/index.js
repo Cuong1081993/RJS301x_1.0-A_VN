b68307980daf7192f1206f61cb1cbec90e698e1a
Cuong <ntqcuong1993@gmail.com>
2023-07-26 11:13:01 +0700

    redux toolkit

---
+++ Lab 15/Lab 15.15/src/store/index.js
@@ -0,0 +1,47 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const initiaCounterState = { counter: 0, showCounter: true, isAuthenticated: false };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState:initiaCounterState,
+  reducers: {
+    
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
+
+const initAuthState ={
+  isAuthenticated : false
+};
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState : initAuthState,
+  reducers:{
+    login(state){
+      state.isAuthenticated = true;
+    },
+    logout(state){
+      state.isAuthenticated = false;
+    }
+  }
+});
+const store = configureStore({
+  reducer: {counter: counterSlice.reducer, auth : authSlice.reducer}
+});
+
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+export default store;
