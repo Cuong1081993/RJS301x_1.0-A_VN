14523b5f4629a75e5dfdfb5d1ead5929ec05ccd5
unknown <ntqcuong1993@gmail.com>
2023-07-31 22:39:44 +0700

    fetch redux

--- Lab 16/Redux-advance/src/store/ui-slice.js
+++ Lab 16/Redux-advance/src/store/ui-slice.js
@@ -1,12 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 const uiSlice = createSlice({
-    name: "ui",
-    initialState: { cartIsVisible: false },
-    reducers: {
-      toggle(state) {
-        state.cartIsVisible = !state.cartIsVisible;
-      },
+  name: "ui",
+  initialState: { cartIsVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
     },
-  });
-  export const uiActions = uiSlice.actions;
-  export default uiSlice;
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message
+      };
+    },
+  },
+});
+export const uiActions = uiSlice.actions;
+export default uiSlice;
