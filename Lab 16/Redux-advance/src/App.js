77c22cceef8ee6d99fed028e43c2a0652e425ca3
Cuong Nguyen <ntqcuong1993@gmail.com>
2023-08-01 16:36:47 +0700

    ok

--- Lab 16/Redux-advance/src/App.js
+++ Lab 16/Redux-advance/src/App.js
@@ -3,8 +3,8 @@ import { Fragment, useEffect } from "react";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-action";
 
 let isInitial = true;
 function App() {
@@ -13,50 +13,17 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
-
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch(
-        `https://meals-3e083-default-rtdb.firebaseio.com/cart.json`,
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed ! ");
-      }
-    
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success !...",
-          message: "Send cart data successfully",
-        })
-      );
-    };
-
-    if(isInitial){
+    dispatch(fetchCartData());
+  }, [dispatch]);
+  useEffect(() => {
+    if (isInitial) {
       isInitial = false;
       return;
+      if (cart.changed) {
+        dispatch(sendCartData(cart));
+      }
     }
-     
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error...",
-          message: "Sending cart data failed ! ",
-        })
-      );
-    });
   }, [cart, dispatch]);
   return (
     <Fragment>
